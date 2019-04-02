1f6fcdd9bd35400170baed933bdb20d5d339b0bc
DeoliJAVACODE <49207146+DeoliJAVACODE@users.noreply.github.com>
2019-04-02 15:14:12 -0300

    Update dejava.java

--- dejava.java
+++ dejava.java
@@ -13,7 +13,6 @@ AndersonJAVACODE
           double price1 = 2100.0;
           double price2 = 650.50;
           double measure = 53.234567;
-          Locale.setDefault(Locale.US);
           System.out.printf("Products:%n%s, wich price is $ %.2f%n", product1, price1);
           System.out.printf("%s, wich price is $ %.2f\n", product2, price2);
           System.out.printf("%nRecord: %d years old, code %d and gender: %c", age, code, gender);
